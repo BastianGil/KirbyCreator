e42e5bfe0e1c6f1a3bde3a5248d6f1f0797a0ea6
Maria Alejandra Renjifo <marialejarenjifo@hotmail.com>
2020-05-06 23:04:43 -0500

    Updates

---
+++ precache-manifest.7defae80fa49936b2779b95aa28c1821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac34325dd3922ddd751f3053e85afdf",
+    "url": "/KirbyCreator/index.html"
+  },
+  {
+    "revision": "06933fb466bbc33f601b",
+    "url": "/KirbyCreator/static/css/main.080079be.chunk.css"
+  },
+  {
+    "revision": "90faa5987a7941076582",
+    "url": "/KirbyCreator/static/js/2.2f0971aa.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/KirbyCreator/static/js/2.2f0971aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06933fb466bbc33f601b",
+    "url": "/KirbyCreator/static/js/main.5d87e2a1.chunk.js"
+  },
+  {
+    "revision": "79f1a56e433a76e4a1fc",
+    "url": "/KirbyCreator/static/js/runtime-main.36c5eacd.js"
+  }
+]);
